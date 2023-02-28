a88ea35170f972a88a8724e26f0d0555880037ec
JamieMJohnson <64426844+JamieMJohnson@users.noreply.github.com>
2023-02-28 20:45:10 +0000

    Update project.py

--- project.py
+++ project.py
@@ -8,13 +8,19 @@ import math
 #Import the data
 data = pd.read_csv("all_data.csv")
 
+#Take the log of the values
+data["log_gdp"] = np.log2(data["GDP"])
+
+#Filter for lower life expectancy
+younger_deaths = data[(data["Life expectancy at birth (years)"] < 65)]
+
+
 #Scatterplot
-sns.scatterplot(data["Life expectancy at birth (years)"], data["GDP"])
+sns.scatterplot(younger_deaths["Life expectancy at birth (years)"], younger_deaths["log_gdp"])
 
 #Find a line of best fit using regplot
-sns.regplot(x="GDP", y="Life expectancy at birth (years)", data=data, order=5)
+sns.regplot(younger_deaths["Life expectancy at birth (years)"], younger_deaths["log_gdp"], order=3)
 
 plt.show()
 plt.clf()
 
-plt.plot(data["Life expectancy at birth (years)"], data["GDP"])
